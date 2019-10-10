b09528ddc68bb4efcd72b26fe6dca3d6d9905261
damianS7 <josepwnz@gmail.com>
2019-10-10 23:57:32 +0200

    Agregado comprobaciones de existencia de ficheros necesarios para el funcionamiento de la aplicacion. De esta forma evitamos excepciones en el futuro.

--- src/main/java/com/chat/client/ClientApplication.java
+++ src/main/java/com/chat/client/ClientApplication.java
@@ -2,6 +2,9 @@ package com.chat.client;
 
 import java.awt.EventQueue;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.chat.client.ui.ClientUI;
 
@@ -9,10 +12,36 @@ public class ClientApplication {
     public static ClientUI ui;
     public static ClientChat client;
     public static Preferences config;
+    public static Logger LOGGER = Logger.getLogger(ClientApplication.class.getName());
 
     public static void main(String[] args) {
-        config = new Preferences(new File(""));
+        LOGGER.log(Level.INFO, "Comprobando configuracion minima ...");
+
+        // Comprobamos si existe el fichero de preferencias del cliente
+        File filePreferences = new File("preferences.properties");
+
+        if (!filePreferences.exists()) {
+            LOGGER.log(Level.INFO,
+                    "El fichero " + filePreferences.getName() + " no existe en " + filePreferences.getAbsolutePath());
+
+            try {
+                filePreferences.createNewFile();
+                LOGGER.log(Level.INFO, filePreferences.getName() + " creado con exito.");
+            } catch (SecurityException e) {
+                LOGGER.log(Level.SEVERE, "No se puede crear el fichero " + filePreferences.getName()
+                        + " porque no se tienen permisos de escritura en " + filePreferences.getAbsolutePath());
+                // No se puede escribir en el directorio
+            } catch (IOException e) {
+                // Error IO
+                e.printStackTrace();
+            }
+        }
+
+        config = new Preferences(filePreferences);
+
         client = new ClientChat();
+
+        LOGGER.log(Level.INFO, "Todo OK. Iniciando cliente ...");
 
         // Inicio de la interfaz grafica en un hilo separado
         EventQueue.invokeLater(new Runnable() {
