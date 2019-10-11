a21f9b18ae2b2cb35a66f82c60d9e330ec9f2af3
damianS7 <josepwnz@gmail.com>
2019-10-11 22:08:17 +0200

    Nuevas clases necesarias para ClientChat

---
+++ src/main/java/com/chat/common/Conversation.java
@@ -0,0 +1,7 @@
+package com.chat.common;
+
+public class Conversation {
+    public String userA;
+    public String userB;
+    public String log;
+}
---
+++ src/main/java/com/chat/common/Room.java
@@ -0,0 +1,8 @@
+package com.chat.common;
+
+public class Room {
+    public String roomName;
+    public String roomDescription;
+    public String roomImage;
+    public String roomModerators;
+}
