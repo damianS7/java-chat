310ee2407b2ec203364f7ba994f5e8340b54be1e
damianS7 <josepwnz@gmail.com>
2019-10-19 22:34:24 +0200

    implementacion inicial del servidor

--- src/main/java/com/chat/server/ServerApplication.java
+++ src/main/java/com/chat/server/ServerApplication.java
@@ -2,15 +2,41 @@ package com.chat.server;
 
 import com.chat.server.ui.ServerUI;
 import java.awt.EventQueue;
+import java.io.File;
+import java.io.IOException;
 
+/*
+ * Esta clase es el "bootstrap" de la aplicacion. Desde esta clase tenemos
+ * acceso a otros elementos de la aplicaccion como la UI, Logger o la
+ * configuracion del servidor.
+ * 
+ * Tambien desde el metodo main se realizan comprobaciones para asegurar
+ * que la aplicacion arranca con los requerimientos minimos para funcionar
+ */
 public class ServerApplication {
     public static ServerUI ui;
+    public static ServerChat server;
+    public static ServerPreferences config;
 
     public static void main(String[] args) {
+	File file = new File("server.properties");
+	
+	// Creacion del fichero de configuracion en caso de no existir
+	if (!file.exists()) {
+	    try {
+		file.createNewFile();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	
+	config = new ServerPreferences(file);
+	ServerApplication.server = new ServerChat();
+	
         // Inicio de la interfaz grafica en un hilo separado
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ui = new ServerUI();
+                ServerApplication.ui = new ServerUI();
             }
         });
     }
---
+++ src/main/java/com/chat/server/ServerChat.java
@@ -0,0 +1,53 @@
+package com.chat.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Esta clase implementa en el servidor metodos concretos para gestionar 
+ * el servidor del chat.  
+ * @author Damian
+ */
+public class ServerChat extends Server {
+    private List<ServerConnection> connections = new ArrayList<ServerConnection>();
+
+    public ServerChat() {
+	setAddress(ServerApplication.config.getAddress());
+	setPort(ServerApplication.config.getPort());
+    }
+    
+    @Override
+    public boolean start() {
+	if (!super.start()) {
+	    return false;
+	}
+
+	ServerApplication.ui.log.addLine("Servidor escuchando en " 
+		+ ServerApplication.config.getAddress() + ":"
+		+ ServerApplication.config.getPort());
+
+	ServerApplication.ui.setStatus("Started.");
+	return true;
+    }
+
+    @Override
+    public boolean stop() {
+	if(!super.stop()) {
+	    return false;
+	}
+	
+	ServerApplication.ui.log.addLine("Servidor detenido.");
+
+	ServerApplication.ui.setStatus("Stopped.");	
+	return true;
+    }
+
+    public void kick() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
