eb1346aeb8a4ada1c4f17bf9d693a5eaacb595ce
damianS7 <josepwnz@gmail.com>
2019-10-21 18:11:59 +0200

    corregido problemas de indentacion

--- src/main/java/com/chat/server/Server.java
+++ src/main/java/com/chat/server/Server.java
@@ -11,10 +11,10 @@ public abstract class Server implements Runnable {
 
     // Direccion donde el servidor aceptara los clientes
     private String address;
-    
+
     // Puerto donde se escuchara
     private int port;
-    
+
     // Socket principal del servidor
     private ServerSocket serverSocket;
 
@@ -23,63 +23,64 @@ public abstract class Server implements Runnable {
 
     // Devuelve el estado actual del servidor. true para el servidor iniciado.
     public boolean isStarted() {
-	return started;
+        return started;
     }
-    
+
     // Cambia el puerto de esucha del servidor
     public void setPort(int port) {
-	this.port = port;
+        this.port = port;
     }
-    
+
     // Cambia la direccion del servidor
     public void setAddress(String address) {
-	this.address = address;
+        this.address = address;
     }
 
     /**
      * Inicia el servidor
+     * 
      * @return Cuando el servidor se inicia correctamente o ya esta iniciado
-     * devuelve <strong>true</strong>. Cuando se produce un error y no se inicia el 
-     * servidor se devuelve <strong>false</strong>.
+     *         devuelve <strong>true</strong>. Cuando se produce un error y no se
+     *         inicia el servidor se devuelve <strong>false</strong>.
      */
     public boolean start() {
-	// Si el servidor ya esta iniciado no se hace nada
-	if (isStarted()) {
-	    return true;
-	}
+        // Si el servidor ya esta iniciado no se hace nada
+        if (isStarted()) {
+            return true;
+        }
 
-	try {
-	    InetAddress addr = InetAddress.getByName(address);
-	    serverSocket = new ServerSocket(port, 0, addr);
-	    started = true;
-	    return true;
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+        try {
+            InetAddress addr = InetAddress.getByName(address);
+            serverSocket = new ServerSocket(port, 0, addr);
+            started = true;
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
-	// Error al iniciar el servidor
-	return false;
+        // Error al iniciar el servidor
+        return false;
     }
 
     /**
      * Detiene el servidor
+     * 
      * @return Cuando el servidor se detiene correctamente o ya estaba detenido
-     * devuelve <strong>true</strong>. Cuando se produce un error y no se detiene
-     * el servidor se devuelve <strong>false</strong>.
+     *         devuelve <strong>true</strong>. Cuando se produce un error y no se
+     *         detiene el servidor se devuelve <strong>false</strong>.
      */
     public boolean stop() {
-	// El servidor no esta iniciado.
-	if (!isStarted()) {
-	    return true;
-	}
+        // El servidor no esta iniciado.
+        if (!isStarted()) {
+            return true;
+        }
 
-	try {
-	    serverSocket.close();
-	    return true;
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-	return false;
+        try {
+            serverSocket.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/main/java/com/chat/server/ServerPreferences.java
+++ src/main/java/com/chat/server/ServerPreferences.java
@@ -9,46 +9,42 @@ import com.chat.common.Preferences;
  * se guardan en un archivo de texto.
  */
 public class ServerPreferences extends Preferences {
-    
+
     // Array con las opciones obligatorias del servidor.
-    private String[][] requiredFields = { 
-	    	{ "address", "127.0.0.1" },
-            	{ "port", "7777" }, 
-            	{ "save_logs", "false" } 
-            };
-    
+    private String[][] requiredFields = { { "address", "127.0.0.1" }, { "port", "7777" }, { "save_logs", "false" } };
+
     public ServerPreferences(File file) {
-	super(file);
-	
-	// Comprobamos que los campos requeridos existan el fichero
-	for (String objectArray[] : requiredFields) {
+        super(file);
+
+        // Comprobamos que los campos requeridos existan el fichero
+        for (String objectArray[] : requiredFields) {
             // No se encuentra el campo en el fichero, se agrega con setProperty
             if (properties.getProperty(objectArray[0]) == null) {
                 setProperty(objectArray[0], objectArray[1]);
             }
         }
 
-	// Guarda los cambios en el fichero de configuracion
+        // Guarda los cambios en el fichero de configuracion
         save();
     }
-    
+
     // Configura una nueva direccion para el servidor
-    public void setAddress (String address) {
-	setProperty("address", address);
+    public void setAddress(String address) {
+        setProperty("address", address);
     }
 
     // Obtiene la direccion que el servidor usa para escuchar
     public String getAddress() {
-	return getProperty("address");
+        return getProperty("address");
     }
-    
+
     // Configura un nuevo puerto para el servidor
     public void setPort(String port) {
-	setProperty("port", port);
+        setProperty("port", port);
     }
-    
+
     // Obtiene el puerto utilizado para escuchar
     public int getPort() {
-	return Integer.parseInt(getProperty("port"));
+        return Integer.parseInt(getProperty("port"));
     }
 }
