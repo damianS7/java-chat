41ea597d864d4855f3ce275e774f19eaad35ee13
damianS7 <josepwnz@gmail.com>
2019-10-21 18:11:15 +0200

    modificacion de la configuracion desde la ui.

--- src/main/java/com/chat/server/ui/ConfigPanel.java
+++ src/main/java/com/chat/server/ui/ConfigPanel.java
@@ -1,15 +1,21 @@
 package com.chat.server.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+
+import com.chat.server.ServerApplication;
+
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 /*
  * Panel que contiene la configuracion de las opciones modificables del
@@ -17,8 +23,8 @@ import javax.swing.JButton;
  */
 
 public class ConfigPanel extends JPanel {
-    private JTextField textField;
-    private JTextField textField_1;
+    private JTextField addressField;
+    private JTextField portField;
 
     public ConfigPanel() {
         setLayout(new BorderLayout(0, 0));
@@ -30,23 +36,34 @@ public class ConfigPanel extends JPanel {
 
         JPanel panel = new JPanel();
         scrollPane.setViewportView(panel);
-        panel.setLayout(new MigLayout("", "[grow,right][grow,right]", "[][][grow][]"));
+        panel.setLayout(new MigLayout("", "[grow,right][grow,fill]", "[][][grow][]"));
 
         JLabel lblAddress = new JLabel("Address");
         panel.add(lblAddress, "cell 0 0,alignx trailing");
 
-        textField = new JTextField();
-        panel.add(textField, "cell 1 0,growx");
-        textField.setColumns(10);
+        addressField = new JTextField(ServerApplication.config.getAddress());
+        addressField.setHorizontalAlignment(SwingConstants.RIGHT);
+        panel.add(addressField, "cell 1 0,growx");
+        addressField.setColumns(10);
 
         JLabel lblPort = new JLabel("Port");
         panel.add(lblPort, "cell 0 1,alignx trailing");
 
-        textField_1 = new JTextField();
-        panel.add(textField_1, "cell 1 1,growx");
-        textField_1.setColumns(10);
+        portField = new JTextField(Integer.toString(ServerApplication.config.getPort()));
+        portField.setHorizontalAlignment(SwingConstants.RIGHT);
+        panel.add(portField, "cell 1 1,alignx right");
+        portField.setColumns(10);
 
         JButton btnSave = new JButton("SAVE");
+        btnSave.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ServerApplication.config.setAddress(addressField.getText());
+                ServerApplication.config.setPort(portField.getText());
+                ServerApplication.config.save();
+            }
+        });
         panel.add(btnSave, "cell 1 3");
     }
 
