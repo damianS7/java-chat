9ab55741b4a0c699aa991e47eef5fbeaec427319
damianS7 <josepwnz@gmail.com>
2019-10-21 16:55:19 +0200

    actualizacion iconos ui

--- src/main/java/com/chat/server/ui/ServerUI.java
+++ src/main/java/com/chat/server/ui/ServerUI.java
@@ -47,7 +47,7 @@ public class ServerUI {
         panelMenu.add(lblAdministration, "cell 0 0 2 1,alignx center");
 
         JLabel label_1 = new JLabel("");
-        label_1.setIcon(new ImageIcon(ServerUI.class.getResource("/javax/swing/plaf/basic/icons/JavaCup16.png")));
+        label_1.setIcon(new ImageIcon(ServerUI.class.getResource("/images/home.png")));
         panelMenu.add(label_1, "cell 0 1");
 
         JButton btnHome = new JButtonLink("Home");
@@ -60,6 +60,7 @@ public class ServerUI {
         panelMenu.add(btnHome, "cell 1 1,alignx left");
 
         JLabel label_2 = new JLabel("");
+        label_2.setIcon(new ImageIcon(ServerUI.class.getResource("/images/rooms.png")));
         panelMenu.add(label_2, "cell 0 2");
 
         JButton btnRooms = new JButtonLink("Rooms");
@@ -73,7 +74,7 @@ public class ServerUI {
         panelMenu.add(btnRooms, "cell 1 2");
 
         JLabel labelAccounts = new JLabel("");
-        labelAccounts.setIcon(new ImageIcon(ServerUI.class.getResource("/javax/swing/plaf/basic/icons/JavaCup16.png")));
+        labelAccounts.setIcon(new ImageIcon(ServerUI.class.getResource("/images/accounts.png")));
         panelMenu.add(labelAccounts, "cell 0 3");
 
         JButton btnAccounts = new JButtonLink("Accounts");
@@ -87,6 +88,7 @@ public class ServerUI {
         panelMenu.add(btnAccounts, "cell 1 3");
 
         JLabel label_4 = new JLabel("");
+        label_4.setIcon(new ImageIcon(ServerUI.class.getResource("/images/privileges.png")));
         panelMenu.add(label_4, "cell 0 4");
 
         JButton btnPrivileges = new JButtonLink("Privileges");
@@ -99,7 +101,7 @@ public class ServerUI {
         panelMenu.add(btnPrivileges, "cell 1 4");
 
         JLabel label = new JLabel("");
-        label.setIcon(new ImageIcon(ServerUI.class.getResource("/javax/swing/plaf/basic/icons/JavaCup16.png")));
+        label.setIcon(new ImageIcon(ServerUI.class.getResource("/images/config.png")));
         panelMenu.add(label, "cell 0 5");
 
         JButton btnLog = new JButtonLink("Log");
@@ -122,11 +124,9 @@ public class ServerUI {
         panelMenu.add(btnConfig, "cell 1 5");
 
         JLabel label_5 = new JLabel("");
+        label_5.setIcon(new ImageIcon(ServerUI.class.getResource("/images/log.png")));
         panelMenu.add(label_5, "cell 0 6");
         panelMenu.add(btnLog, "cell 1 6");
-
-        JLabel label_6 = new JLabel("");
-        panelMenu.add(label_6, "cell 0 7");
 
         JButton btnStart = new JButton("Start");
         btnStart.addActionListener(new ActionListener() {
