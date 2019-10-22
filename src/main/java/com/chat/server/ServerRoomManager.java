f447c287702e8ec67e351f74fe61e8a132b59c15
damianS7 <josepwnz@gmail.com>
2019-10-22 13:16:03 +0200

    Clase para gestionar las salas desde el servidor

---
+++ src/main/java/com/chat/server/ServerRoomManager.java
@@ -0,0 +1,16 @@
+package com.chat.server;
+
+import com.chat.common.Room;
+
+/**
+ * Esta clase gestiona la sala. Entradas y salidas.
+ * 
+ * @author Damian
+ */
+public class ServerRoomManager {
+    private Room room;
+
+    public void kick() {
+
+    }
+}
