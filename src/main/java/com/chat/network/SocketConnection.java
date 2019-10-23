2552218822ff8a693177e1c8a015240ce6c80629
damianS7 <josepwnz@gmail.com>
2019-10-23 22:44:46 +0200

    nuevos metodos implementados

--- src/main/java/com/chat/network/SocketConnection.java
+++ src/main/java/com/chat/network/SocketConnection.java
@@ -1,24 +1,32 @@
 package com.chat.network;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
+
+import com.chat.network.packets.Packet;
 
 /**
  */
-public abstract class SocketConnection implements Connection, ConnectionIO {
-    protected Socket socket;
+public abstract class SocketConnection implements Runnable, BasicSocket, SocketIO, PacketHandler {
     protected String address;
+    protected Socket socket;
 
     public SocketConnection(Socket socket) {
         this.socket = socket;
         this.address = socket.getInetAddress().getHostAddress();
+        new Thread(this).start();
     }
 
     public String getAddress() {
         return address;
     }
 
-    public Socket getConnectionSocket() {
+    public Socket getSocket() {
         return socket;
     }
 
@@ -38,7 +46,51 @@ public abstract class SocketConnection implements Connection, ConnectionIO {
                 e.printStackTrace();
             }
         }
-
     }
 
+    @Override
+    public void writePacket(Packet packet) {
+        try {
+            OutputStream os = socket.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(packet);
+            oos.flush();
+        } catch (SocketException e) {
+            // Socket cerrado, intentar reconexion ?? desconectar cliente
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Packet readPacket() {
+        Packet packet = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            packet = (Packet) ois.readObject();
+            return packet;
+        } catch (SocketException e) {
+            // Conexion perdida
+        } catch (EOFException e) {
+            // close();
+            // conexion perdida
+            // timeout 0
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        close();
+        return null;
+    }
+
+    @Override
+    public void run() {
+        while (isAlive()) {
+            Packet packet = readPacket();
+            processPacket(packet);
+        }
+    }
 }
