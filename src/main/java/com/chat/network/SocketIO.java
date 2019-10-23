d72e509d26c49afbb9557e5de82e6b1c560dba32
damianS7 <josepwnz@gmail.com>
2019-10-23 22:33:26 +0200

    comentario actualizado

--- src/main/java/com/chat/network/SocketIO.java
+++ src/main/java/com/chat/network/SocketIO.java
@@ -2,16 +2,24 @@ package com.chat.network;
 
 import com.chat.network.packets.Packet;
 
+/**
+ * Implementacion de operaciones de escritura y lectura sobre un socket
+ * 
+ * @author Damian
+ *
+ */
 public interface SocketIO {
     /**
-     * Envia un objeto a traves del socket
+     * Envio de paquetes a traves de un socket
      * 
-     * @param p El objeto a enviar debe ser una instancia de DataPacket
-     * @return True si envio el paquete
-     * @throws ConnectionLostException si la conexion se ha perdido o el socket
-     *                                 fue cerrado
+     * @param packet
      */
     public void writePacket(Packet packet);
 
+    /**
+     * Si se recibe un paquete a traves del socket se capturara en este metodo
+     * 
+     * @return
+     */
     public Packet readPacket();
 }
