c5d8776cccee9af6988a51e4626fb6fd5be5dfc4
damianS7 <josepwnz@gmail.com>
2019-10-25 15:58:06 +0200

    ahora es posible modificar descripciones de rooms

--- src/main/java/com/chat/server/ui/RoomsPanel.java
+++ src/main/java/com/chat/server/ui/RoomsPanel.java
@@ -1,8 +1,9 @@
 package com.chat.server.ui;
 
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,10 +14,8 @@ import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 
 import com.chat.common.BasicRoom;
@@ -26,124 +25,154 @@ import net.miginfocom.swing.MigLayout;
 
 public class RoomsPanel extends JPanel {
     private static final long serialVersionUID = -7985614931111845166L;
-    private JList<String> list;
-    private JTextField roomName;
-    private JSpinner maxUsers;
-    private JTextArea description;
-    private JTextField mod;
     private DefaultListModel<String> listModel;
+    private JList<String> roomList;
+    private JTextField roomNameField;
+    private JTextArea roomDescriptionField;
 
     public RoomsPanel() {
-	super();
-	setLayout(new MigLayout("", "[200px:n,right][grow][grow,fill]", "[][][][grow][][][]"));
+        super();
+        setLayout(new MigLayout("", "[grow,fill][200px:n,right][grow]", "[][][][grow][]"));
 
-	JScrollPane scrollPane = new JScrollPane();
-	add(scrollPane, "cell 2 0 1 4,grow");
+        JScrollPane scrollPane = new JScrollPane();
+        add(scrollPane, "cell 0 0 1 4,grow");
 
-	listModel = new DefaultListModel<String>();
-	list = new JList<String>();
-	list.setModel(listModel);
-	loadRooms();
+        listModel = new DefaultListModel<String>();
+        roomList = new JList<String>();
+        roomList.addMouseListener(new MouseListener() {
 
-	scrollPane.setViewportView(list);
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-	JLabel lblRoom = new JLabel("Room");
-	add(lblRoom, "cell 0 0,alignx right");
+            }
 
-	roomName = new JTextField();
-	roomName.setHorizontalAlignment(SwingConstants.RIGHT);
-	add(roomName, "cell 1 0,growx");
-	roomName.setColumns(10);
+            @Override
+            public void mousePressed(MouseEvent e) {
+                showRoomInfo();
+            }
 
-	JLabel lblMaxUsers = new JLabel("Max users");
-	add(lblMaxUsers, "cell 0 1,alignx right");
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-	maxUsers = new JSpinner();
-	maxUsers.setModel(new SpinnerNumberModel(20, 1, 1000, 1));
-	add(maxUsers, "cell 1 1,growx");
+            }
 
-	JLabel lblDescription = new JLabel("Description");
-	add(lblDescription, "cell 0 2");
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-	JScrollPane scrollPane_1 = new JScrollPane();
-	add(scrollPane_1, "cell 1 2 1 2,grow");
+            }
 
-	description = new JTextArea();
-	scrollPane_1.setViewportView(description);
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-	JButton btnDelete = new JButton("Delete");
-	btnDelete.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		String name = list.getSelectedValue();
-		if (name != null) {
-		    int reply = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete?", "delete?",
-			    JOptionPane.YES_NO_OPTION);
-		    if (reply == JOptionPane.YES_OPTION) {
-			try {
-			    ServerApplication.roomDatabase.deleteRoom(name);
-			} catch (SQLException e1) {
-			    e1.printStackTrace();
-			}
-			listModel.remove(list.getSelectedIndex());
-		    }
-		}
-	    }
-	});
+            }
+        });
+        roomList.setModel(listModel);
+        loadRooms();
 
-	add(btnDelete, "cell 2 4,growx");
+        scrollPane.setViewportView(roomList);
 
-	JButton btnAddmodify = new JButton("Add/Modify");
-	btnAddmodify.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		String name = roomName.getText();
-		String desc = description.getText();
-		// int maxu = Integer.parseInt(maxUsers.getModel().getValue().toString());
+        JLabel lblRoom = new JLabel("Room");
+        add(lblRoom, "cell 1 0,alignx right");
 
-		if (name.isEmpty() || desc.isEmpty()) {
-		    JOptionPane.showMessageDialog(null, "Room name/description cannot be empty!");
-		    return;
-		}
+        roomNameField = new JTextField();
+        roomNameField.setHorizontalAlignment(SwingConstants.RIGHT);
+        add(roomNameField, "cell 2 0,growx");
+        roomNameField.setColumns(10);
 
-		BasicRoom r = new BasicRoom(name, desc);
-		String[] mods = mod.getText().split(",");
+        JLabel lblDescription = new JLabel("Description");
+        add(lblDescription, "cell 1 1");
 
-		for (String mod : mods) {
-		    // r.addRoomModerator(mod.trim());
-		}
+        JScrollPane scrollPane_1 = new JScrollPane();
+        add(scrollPane_1, "cell 2 1 1 3,grow");
 
-		try {
-		    ServerApplication.roomDatabase.addRoom(r);
-		} catch (SQLException e1) {
-		    e1.printStackTrace();
-		}
-		listModel.addElement(name);
-	    }
-	});
+        roomDescriptionField = new JTextArea();
+        scrollPane_1.setViewportView(roomDescriptionField);
 
-	JLabel lblMod = new JLabel("Mod");
-	add(lblMod, "cell 0 4,alignx right");
+        JButton btnDelete = new JButton("Delete");
+        btnDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String roomName = roomList.getSelectedValue();
 
-	mod = new JTextField();
-	mod.setHorizontalAlignment(SwingConstants.RIGHT);
-	add(mod, "cell 1 4,growx");
-	mod.setColumns(10);
-	add(btnAddmodify, "cell 1 5,growx");
+                if (roomName == null) {
+                    return;
+                }
 
-	JPanel panel = new JPanel();
-	FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-	flowLayout.setAlignment(FlowLayout.RIGHT);
-	add(panel, "cell 1 6,grow");
+                int reply = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete?", "delete?",
+                        JOptionPane.YES_NO_OPTION);
 
-	setVisible(true);
+                if (reply == JOptionPane.YES_OPTION) {
+                    try {
+                        ServerApplication.roomDatabase.deleteRoom(roomName);
+                        listModel.remove(roomList.getSelectedIndex());
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                        JOptionPane.showInternalMessageDialog(null,
+                                "No se pudo" + " borrar debido a un problema con interno.");
+                    }
+                }
+            }
+        });
+
+        JButton btnAddmodify = new JButton("Add/Modify");
+        btnAddmodify.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String roomName = roomNameField.getText();
+                String roomDesc = roomDescriptionField.getText();
+
+                if (roomName.isEmpty() || roomDesc.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Room name/description cannot be empty!");
+                    return;
+                }
+
+                BasicRoom room = null;
+
+                try {
+                    room = ServerApplication.roomDatabase.getRoom(roomName);
+
+                    // UPDATE
+                    if (room != null) {
+                        ServerApplication.roomDatabase.updateRoom(roomName, "DESCRIPTION", roomDesc);
+                        return;
+                    }
+
+                    // INSERT
+                    room = new BasicRoom(roomName, roomDesc);
+                    ServerApplication.roomDatabase.addRoom(room);
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+
+                listModel.addElement(roomName);
+            }
+        });
+
+        add(btnAddmodify, "cell 2 4,growx");
+        add(btnDelete, "cell 0 4,growx");
+        setVisible(true);
+    }
+
+    private void showRoomInfo() {
+        String roomName = roomList.getSelectedValue();
+        try {
+            BasicRoom room = ServerApplication.roomDatabase.getRoom(roomName);
+            roomNameField.setText(room.name);
+            roomDescriptionField.setText(room.description);
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
     }
 
     private void loadRooms() {
-	List<BasicRoom> rooms = ServerApplication.roomDatabase.getRooms();
-	for (BasicRoom row : rooms) {
-	    listModel.addElement(row.name);
-	}
+        List<BasicRoom> rooms = ServerApplication.roomDatabase.getRooms();
+        for (BasicRoom room : rooms) {
+            listModel.addElement(room.name);
+        }
     }
 
 }
